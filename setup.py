3d39722b0baed6aac098cbdc492e30cb4e91c48e
nullp0inter <nullp0inter@tombstone>
2018-07-19 16:14:00 -0400

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = 'fileutils',
+    author = 'Nullp0inter',
+    version = '0.0.1'
+    description = 'Library to bring some file niceness to your life',
+    long_description = 'Adds file related functions for handling copying of files'\
+                    + ' some hashing options and more.',
+
+
+    packages = find_packages()
+)
